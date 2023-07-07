526122dcdb3da180bdb3e19eee4044b49ee5c17c
Rus1anGU <95142486+Rus1anGU@users.noreply.github.com>
2023-07-07 21:54:24 +0300

    v1

--- src/index.js
+++ src/index.js
@@ -1,103 +1,110 @@
 document.addEventListener('DOMContentLoaded', function() {
-  const calculationDisplay = document.getElementById('calculation');
-  const resultDisplay = document.getElementById('result');
-  const buttons = document.querySelectorAll('.buttons button');
-  const enterButton = document.querySelector('.Enter');
-  const backButton = document.querySelector('.back');
-  const delButton = document.querySelector('.del');
-  const historyContainer = document.getElementById('history');
+  function calculator(activCalkulating, resultCalculation, historySelector){
 
-  let calculation = '';
-  let result = 0;
-  let history = [];
+    const calculationDisplay = document.getElementById(activCalkulating);
+    const resultDisplay = document.getElementById(resultCalculation);
+    const historyContainer = document.getElementById(historySelector);
 
-  buttons.forEach(function(button) {
-    button.addEventListener('click', function() {
-      const value = button.getAttribute('data-value');
-      appendToCalculation(value);
-    });
-  });
+    const enterButton = document.querySelector('.Enter');
+    const backButton = document.querySelector('.back');
+    const delButton = document.querySelector('.del');
 
-  enterButton.addEventListener('click', function() {
-    calculate();
-  });
+    const buttons = document.querySelectorAll('.buttons button');
 
-  backButton.addEventListener('click', function() {
-    deleteLastCharacter();
-  });
+    let calculation = '';
+    let result = 0;
+    let history = [];
 
-  delButton.addEventListener('click', function() {
-    clearCalculation();
-  });
-
-  function appendToCalculation(value) {
-    if (value === 'result') {
-      calculation += result;
-    } else {
-      calculation += value;
-    }
-    updateCalculation();
-  }
-
-  function deleteLastCharacter() {
-    calculation = calculation.slice(0, -1);
-    updateCalculation();
-  }
-
-  function clearCalculation() {
-    calculation = '';
-    result = 0;
-    updateCalculation();
-    resultDisplay.textContent = result;
-    clearHistory();
-  }
-
-  function calculate() {
-    result = eval(calculation);
-    resultDisplay.textContent = result;
-
-    if (result === Infinity) {
-      calculation = '0';
-    } else {
-      calculation = result.toString();
-    }
-
-    saveCalculation(calculation, result);
-    updateCalculation();
-  }
-
-  function updateCalculation() {
-    calculationDisplay.textContent = calculation;
-  }
-
-  function saveCalculation(expression, result) {
-    const calculation = {
-      expression: expression,
-      result: result
-    };
-    history.push(calculation);
-    displayHistory();
-  }
-
-  function displayHistory() {
-    historyContainer.innerHTML = '';
-    history.forEach(function(calculation) {
-      const calculationItem = document.createElement('div');
-      calculationItem.textContent = `${calculation.expression} = ${calculation.result}`;
-      calculationItem.addEventListener('click', function() {
-        calculationClicked(calculation.expression);
+    buttons.forEach(function(button) {
+      button.addEventListener('click', function() {
+        const value = button.getAttribute('data-value');
+        appendToCalculation(value);
       });
-      historyContainer.appendChild(calculationItem);
     });
-  }
 
-  function calculationClicked(expression) {
-    calculation = expression;
-    updateCalculation();
-  }
+    function appendToCalculation(value) {
+      if (value === 'result') {
+        calculation += result;
+      } else {
+        calculation += value;
+      }
+      updateCalculation();
+    }
 
-  function clearHistory() {
-    history = [];
-    historyContainer.innerHTML = '';
+    function updateCalculation() {
+      calculationDisplay.textContent = calculation;
+    }
+
+    //------//
+    enterButton.addEventListener('click', function() {
+      calculate();
+    });
+    backButton.addEventListener('click', function() {
+      deleteLastCharacter();
+    });
+    delButton.addEventListener('click', function() {
+      clearCalculation();
+    });
+    //------//
+
+    function calculate() {
+      result = eval(calculation);
+      resultDisplay.textContent = result;
+
+      if (result === Infinity) {
+        calculation = '0';
+      } else {
+        calculation = result.toString();
+      }
+
+      saveCalculation(calculation, result);
+      updateCalculation();
+    }
+
+    function deleteLastCharacter() {
+      calculation = calculation.slice(0, -1);
+      updateCalculation();
+    }
+
+    function clearCalculation() {
+      calculation = '';
+      result = 0;
+      updateCalculation();
+      resultDisplay.textContent = result;
+      clearHistory();
+    }
+
+    
+    function saveCalculation(expression, result) {
+      const calculation = {
+        expression: expression,
+        result: result
+      };
+      history.push(calculation);
+      displayHistory();
+    }
+
+    function displayHistory() {
+      historyContainer.innerHTML = '';
+      history.forEach(function(calculation) {
+        const calculationItem = document.createElement('div');
+        calculationItem.textContent = `${calculation.expression} = ${calculation.result}`;
+        calculationItem.addEventListener('click', function() {
+          calculationClicked(calculation.expression);
+        });
+        historyContainer.appendChild(calculationItem);
+      });
+    }
+
+    function calculationClicked(expression) {
+      calculation = expression;
+      updateCalculation();
+    }
+
+    function clearHistory() {
+      history = [];
+      historyContainer.innerHTML = '';
+    }
   }
+  calculator('calculation', 'result', 'history')
 });
